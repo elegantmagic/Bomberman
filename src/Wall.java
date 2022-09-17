06f5fbd1154065c30c5c6b24f0900ca752bc52d3
elegantmagic <21020637@vnu.edu.vn>
2022-09-17 16:02:29 +0700

    Kien Hoang

--- src/Wall.java
+++ src/Wall.java
@@ -3,27 +3,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
-
 public class Wall extends JPanel {
-    BufferedImage image;
-    public void draw() {
-
-        this.image = new BufferedImage(31*3*16, 13*3*16, BufferedImage.TYPE_INT_RGB);
-        BufferedImage s = null;
-        try{
-            s = ImageIO.read(getClass().getResource("/image/wall/Wall.png"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        Graphics2D graphics2D = (Graphics2D) this.image.getGraphics();
-        graphics2D.setBackground(new Color(56, 133, 0));
-        graphics2D.fillRect(0, 0, 31*3*16,13*3*16);
-        graphics2D.drawImage(s,0, 0, 16*3, 16*3, null);
-
-            Graphics graphics = getGraphics();
-            graphics.drawImage(this.image, 0, 0, 31*3*16, 13*3*16, null);
-            graphics.dispose();
-    }
 
 }
--- src/Window.java
+++ src/Window.java
@@ -1,13 +1,19 @@
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.Objects;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
-public class Window extends  JPanel{
+public class Window extends JPanel implements Runnable {
+    boolean isRunning;
+    Thread thread;
+    public static int width = 31;
+    public static int height = 13;
+    int[][] scene;
+
     public Window() {
-        int width = 31;
-        int height = 13;
         setPreferredSize(new Dimension(width * 16 * 3, height * 16 * 3));
     }
-
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Bomberman");
@@ -18,4 +24,85 @@ public class Window extends  JPanel{
         frame.pack();
         frame.setVisible(true);
     }
+
+    @Override
+    public void addNotify() {
+        super.addNotify();
+        if (thread == null) {
+            thread = new Thread(this);
+            isRunning = true;
+            thread.start();
+        }
+    }
+
+    BufferedImage image, wall, bomber, all, block;
+
+    public void start() {
+        try {
+            image = new BufferedImage(31 * 3 * 16, 13 * 3 * 16, BufferedImage.TYPE_INT_RGB);
+            all = ImageIO.read(Objects.requireNonNull(getClass().getResource("/image/img.png")));
+            wall = all.getSubimage(4 * 16, 3 * 16, 16, 16);
+            bomber = all.getSubimage(4 * 16, 0, 16, 16);
+            block = all.getSubimage(3 * 16, 3 * 16, 16, 16);
+
+            scene = new int[][]{
+                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                    {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
+                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                    {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
+                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                    {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
+                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                    {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
+                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                    {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
+                    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+            };
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void draw() {
+        Graphics2D graphics2D = (Graphics2D) image.getGraphics();
+        graphics2D.setColor(new Color(56, 133, 0));
+        graphics2D.fillRect(0, 0, 31 * 16 * 3, 13 * 16 * 3);
+
+        int size = 16 * 3;
+        for (int i = 0; i < Window.width; i++) {
+            for (int j = 0; j < Window.height; j++) {
+                if (scene[j][i] == 1) {
+                    graphics2D.drawImage(block, i * size, j * size, size, size, null);
+                }
+            }
+        }
+
+        graphics2D.drawImage(bomber, 16 * 3, 16 * 3, 16 * 3, 16 * 3, null);
+
+        Graphics graphics = getGraphics();
+        graphics.drawImage(this.image, 0, 0, 31 * 3 * 16, 13 * 3 * 16, null);
+        graphics.dispose();
+    }
+
+    public void update() {
+
+    }
+
+    @Override
+    public void run() {
+        try {
+            requestFocus();
+            start();
+            while (isRunning) {
+                update();
+                draw();
+                Thread.sleep(1000 / 60);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
+
