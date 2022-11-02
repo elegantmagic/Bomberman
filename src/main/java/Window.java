10345fef153e17a849833c7f78c4922596e2f1ea
elegantmagic <21020637@vnu.edu.vn>
2022-11-02 13:28:37 +0700

    Kien

--- src/main/java/Window.java
+++ src/main/java/Window.java
@@ -1,20 +1,33 @@
 //package main.java;
 
 import javazoom.jl.player.Player;
+import javazoom.jl.player.advanced.AdvancedPlayer;
 
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.io.*;
 
+import javax.swing.JPanel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 public class Window extends JPanel implements Runnable {
     boolean isRunning;
     Thread thread;
     public static int width = 31;
     public static int height = 13;
+
+    private boolean startScreen = false;
 
     private static JFrame frame;
 
@@ -24,12 +37,37 @@ public class Window extends JPanel implements Runnable {
     private boolean boom = false;
     private int boomX, boomY;
 
+    private JFrame mainFrame;
+    private JLabel headerLabel;
+    private JLabel statusLabel;
+    private JPanel controlPanel;
 
     public Window() {
         setPreferredSize(new Dimension(width * 16 * 3, height * 16 * 3));
+        prepareGUI();
     }
+    private void prepareGUI() {
 
+    }
+    class CustomMouseListener implements MouseListener {
+        public void mouseClicked(MouseEvent e) {
+            if(758 <= e.getX() && e.getX() < 927 && 495 < e.getY() && e.getY() < 527) {
+                startScreen = true;
+            }
+        }
 
+        public void mousePressed(MouseEvent e) {
+        }
+
+        public void mouseReleased(MouseEvent e) {
+        }
+
+        public void mouseEntered(MouseEvent e) {
+        }
+
+        public void mouseExited(MouseEvent e) {
+        }
+    }
     public static void main(String[] args) {
         frame = new JFrame("Bomberman");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -44,12 +82,11 @@ public class Window extends JPanel implements Runnable {
             @Override
             public void run() {
                 try {
-                    FileInputStream file = new FileInputStream("music.mp3");
-                    Player play = new Player(file);
-                    play.play();
-                    if(play.isComplete()) {
+                    do {
+                        FileInputStream file = new FileInputStream("C:\\Users\\Admin\\Documents\\GitHub\\Bomberman\\music.mp3");
+                        AdvancedPlayer play = new AdvancedPlayer(file);
                         play.play();
-                    }
+                    } while (true);
                 } catch (Exception e) {
                     System.out.println(e);
                 }
@@ -71,17 +108,20 @@ public class Window extends JPanel implements Runnable {
     private BufferedImage image;
     private Bomber bomber;
     private TileMap tilemap;
-
+    private BufferedImage StartMenu;
     public void start() {
         try {
+            StartMenu = ImageIO.read(new File("image/start.jpg"));
+            Global.StartMenu = StartMenu;
             BufferedImage all = ImageIO.read(new File("image/img.png"));
+
             Global.all = all;
 
 			bomber = new Bomber(all);
             Global.bomber = bomber;
-            
 
             frame.addKeyListener(bomber);
+            frame.addMouseListener(new CustomMouseListener());
             BufferedImage tileset[] = {
                     all.getSubimage(0, 4 * 16, 16, 16),
                     all.getSubimage(3 * 16, 3 * 16, 16, 16),
@@ -98,14 +138,6 @@ public class Window extends JPanel implements Runnable {
             BombItem.setIcon(all.getSubimage(0* Global.tileSize, 14 * Global.tileSize, Global.tileSize, Global.tileSize));
             FlameItem.setIcon(all.getSubimage(1* Global.tileSize, 14 * Global.tileSize, Global.tileSize, Global.tileSize));
             SpeedItem.setIcon(all.getSubimage(2 * Global.tileSize, 14 * Global.tileSize, Global.tileSize, Global.tileSize));
-
-
-
-
-
-
-
-
 
 
             image = new BufferedImage(tilemap.getWidth() * 3 * 16, tilemap.getHeight() * 3 * 16, BufferedImage.TYPE_INT_RGB);
@@ -126,10 +158,14 @@ public class Window extends JPanel implements Runnable {
         graphics2D.setColor(new Color(56, 133, 0));
         graphics2D.fillRect(0, 0, 31 * 16 * 3, 13 * 16 * 3);
 
-        tilemap.draw();
-        bomber.draw();
-        for (Drawable d : Global.drawables) {
-            d.draw();
+        if(!startScreen) {
+            graphics2D.drawImage(StartMenu, 0, 0, 31 * 16 * 3, 13 * 16 * 3, null);
+        } else {
+            tilemap.draw();
+            bomber.draw();
+            for (Drawable d : Global.drawables) {
+                d.draw();
+            }
         }
 
         Graphics graphics = getGraphics();
